4bfff31edf6eb31086c733c0a8a82376ffce3504
Danielle Tomlinson <dani@tomlinson.io>
2019-03-22 10:49:33 +0000

    plan/apply on CircleCI

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -9,13 +9,8 @@ terraform {
   }
 }
 
-provider "cloudflare" {
-  email = "${var.cloudflare_email}"
-  token = "${var.cloudflare_token}"
-}
+provider "cloudflare" {}
 
 provider "scaleway" {
-  organization = "${var.scaleway_organization_id}"
-  token        = "${var.scaleway_token}"
-  region       = "${var.scaleway_region}"
+  region = "par1"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "cloudflare_email" {}
-variable "cloudflare_token" {}
-
-variable "scaleway_organization_id" {}
-variable "scaleway_token" {}
-
 variable "scaleway_region" {
   default = "par1"
 }
