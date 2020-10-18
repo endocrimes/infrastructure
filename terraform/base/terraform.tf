cacbc7801b036b72573f97f440fadd613f4f0730
Danielle Lancashire <dani@builds.terrible.systems>
2020-10-18 19:28:09 +0200

    0.13, take two

--- terraform/base/terraform.tf
+++ terraform/base/terraform.tf
@@ -5,11 +5,21 @@ terraform {
     region  = "us-east-1"
     profile = "personal"
   }
+
+  required_providers {
+    scaleway = {
+      source = "scaleway/scaleway"
+    }
+
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+  }
 }
 
-provider "cloudflare/cloudflare" {}
+provider "cloudflare" {}
 
-provider "scaleway/scaleway" {
+provider "scaleway" {
   region  = "fr-par"
   zone    = "fr-par-1"
   version = "~> 1.11"
